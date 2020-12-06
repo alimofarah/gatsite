81c7fa058d7e9d9dc36ca937a15a2e00bb9b1a93
alimofarah <alimofarah248@gmail.com>
2020-12-06 15:48:05 +0330

    Updates

---
+++ component---src-templates-page-js-87d82eff5dbb595b8086.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{sweJ:function(e,n,t){"use strict";t.r(n),t.d(n,"pageQuery",(function(){return l}));var a=t("q1tI"),r=t.n(a);n.default=function(e){var n=e.data.wpgraphql.page,t=n.title,a=n.content;return r.a.createElement(r.a.Fragment,null,r.a.createElement("h1",null,t),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:a}}))};var l="1211387553"}}]);
+//# sourceMappingURL=component---src-templates-page-js-87d82eff5dbb595b8086.js.map
